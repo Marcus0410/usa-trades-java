1854b76e1958aa1da39d6879b93a31d76e278f09
marcusri <marcusri@uio.no>
2024-10-28 08:33:54 +0100

    started implementing auto 10bps comm for 415874

--- demo/src/main/java/com/usatrades/Main.java
+++ demo/src/main/java/com/usatrades/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // String clientPath = "J:\\BackOfficeApps\\USA-booking\\kunder.csv";
         // String securitiesPath = "J:\\BackOfficeApps\\USA-booking\\papirer.csv";
-        String clientPath = "kunder.csv";
-        String securitiesPath = "papirer.csv";
+        String clientPath = "./kunder.csv";
+        String securitiesPath = "./papirer.csv";
 
         File clientFile = new File(clientPath);
         File securitiesFile = new File(securitiesPath);
